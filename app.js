6723389d5abea7d7934fe5930c5f0aee75f4d2e2
nibo164 <warimiga164@gmail.com>
2025-02-28 14:42:07 +0900

    [update]部屋分けを実装

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ let http = require("http").Server(app);
 let io = require("socket.io")(http);
 const PORT = process.env.PORT || 7000;
 
+//ロビーにいるユーザーリスト
+let waitingUsers_lobby = [];
+
+//対戦待ちのユーザーリスト
+let waitingUsers_battle = [];
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
@@ -14,18 +20,85 @@ app.use(express.static(__dirname));
 //接続確立時
 io.on("connection", (socket) => {
   console.log("a user connected");
+  socket.emit("connected_to_client");
 
-  //ゲーム開始時
-  io.emit("startGame_to_client", { targetWord: chooseWord() });
+  //ユーザーログイン時
+  socket.on("login_to_server", (data) => {
+    console.log(data.userName + "がログインしました");
+    socket.userName = data.userName;
+    socket.emit("loginSuccess_to_client", { userName: data.userName });
+  });
 
+  //ロビー入室時
+  socket.on("joinLobby_to_server", (data) => {
+    console.log(data.userName + "がロビーに入室しました");
+    waitingUsers_lobby.push(socket);
+    socket.join("lobby");
+    socket.emit("lobbyJoined_to_client");
+    io.to("lobby").emit("updateLobbyLog_to_client", {
+      userName: data.userName,
+      state: "joinedLobby",
+    });
+
+    //対戦待ち時
+    socket.on("battleWait_to_server", () => {
+      console.log(socket.userName + "が対戦待ちになりました");
+      io.to("lobby").emit("updateLobbyLog_to_client", {
+        userName: socket.userName,
+        state: "battleWaiting",
+      });
+      if (waitingUsers_battle.length > 0) {
+        //対戦待ちのユーザーが存在する場合
+        let opponent = waitingUsers_battle.shift();
+        let room = "room_" + socket.userName + "_" + opponent.userName;
+        socket.join(room);
+        opponent.join(room);
+        io.to("lobby").emit("updateLobbyLog_to_client", {
+          userName: socket.userName,
+          opponentName: opponent.userName,
+          state: "battleStarted",
+        });
+
+        //ゲーム開始処理
+        io.to(room).emit("startGame_to_client", {
+          targetWord: chooseWord(),
+          nextTargetWord: chooseWord(),
+        });
+
+        console.log(
+          socket.userName + "と" + opponent.userName + "が対戦を開始しました"
+        );
+      } else {
+        //対戦待ちのユーザーが存在しない場合
+        waitingUsers_battle.push(socket);
+        socket.join("battleWait");
+      }
+    });
+  });
+
+  /*
+  //ゲーム開始
+  socket.emit("startGame_to_client", {
+    targetWord: chooseWord(),
+    nextTargetWord: chooseWord(),
+  });
+  */
   //次の単語を送信
   socket.on("nextWord_to_server", () => {
-    io.emit("nextWord_to_client", { targetWord: chooseWord() });
+    socket.emit("nextWord_to_client", { targetWord: chooseWord() });
   });
 
   //接続切断時
   socket.on("disconnect", () => {
     console.log("user disconnected");
+
+    //ロビー、対戦待ちそれぞれのリストからユーザーを削除
+    waitingUsers_lobby = waitingUsers_lobby.filter(
+      (user) => user.userName != socket.userName
+    );
+    waitingUsers_battle = waitingUsers_battle.filter(
+      (user) => user.userName != socket.userName
+    );
   });
 });
 
@@ -36,17 +109,7 @@ http.listen(PORT, () => {
 //単語リスト
 let json = fs.readFileSync("wordList.json", "utf8");
 let wordList = JSON.parse(json);
-// let wordList = [
-//   "session",
-//   "player",
-//   "server",
-//   "apple",
-//   "sky",
-//   "opened",
-//   "error",
-// ];
 
 function chooseWord() {
-  // return wordList[parseInt(Math.random() * wordList.length)];
   return wordList.words[parseInt(Math.random() * wordList.words.length)];
 }
--- client.js
+++ client.js
@@ -1,23 +1,59 @@
 let socket = io();
 
-// ゲームの初期設定
-const canvas = document.getElementById("game_canvas");
-const context = canvas.getContext("2d");
+let canvas, canvasWrapper, context;
 
-canvas.width = 800;
-canvas.height = 400;
-
-let isGameRunning = true;
-let targetWord, enteredChar, currentIdx;
-let isWordSet = false;
+let isGameRunning = true,
+  isWordSet = false,
+  isNextWordSet = false,
+  isPlayerReady = false;
+let targetWord, nextTargetWord, enteredChar, currentIdx;
 
 document.addEventListener("DOMContentLoaded", () => {
-  // ゲーム開始時
+  //接続確立時
+  socket.on("connected_to_client", () => {
+    createLoginForm();
+  });
+
+  //ログイン成功時
+  socket.on("loginSuccess_to_client", (data) => {
+    socket.emit("joinLobby_to_server", { userName: data.userName });
+  });
+
+  //ロビー入室時
+  socket.on("lobbyJoined_to_client", () => {
+    document.getElementsByTagName("h1")[0].innerText = "ロビー";
+    createLobbyLog();
+    createBattleWaitButton();
+  });
+
+  //ロビーのログ更新
+  socket.on("updateLobbyLog_to_client", (data) => {
+    if (data.state == "joinedLobby") {
+      //ロビー入室時
+      updateLobbyLog(data.userName + "がロビーに入室しました");
+    } else if (data.state == "battleWaiting") {
+      //対戦待ち時
+      updateLobbyLog(data.userName + "が対戦相手を待っています");
+    } else if (data.state == "battleStarted") {
+      //対戦開始時
+      updateLobbyLog(
+        data.userName + "と" + data.opponentName + "が対戦を開始しました"
+      );
+    }
+  });
+
+  // 対戦開始
   socket.on("startGame_to_client", (data) => {
+    document.getElementById("lobby_log").style.display = "none";
+    document.getElementsByTagName("h1")[0].innerText = "対戦中";
+    document.getElementById("battle_wait_button").style.display = "none";
+    createGameCanvas();
     document.addEventListener("keydown", handleKeyPress);
+    nextTargetWord = data.nextTargetWord;
     updateWord(data);
     gameLoop();
   });
+
   // 次の単語を受信
   socket.on("nextWord_to_client", (data) => {
     updateWord(data);
@@ -25,10 +61,12 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function updateWord(data) {
-  targetWord = data.targetWord;
+  targetWord = nextTargetWord;
+  isWordSet = true;
+  nextTargetWord = data.targetWord;
+  isNextWordSet = true;
   enteredChar = "";
   currentIdx = 0;
-  isWordSet = true;
 }
 
 function handleKeyPress(event) {
@@ -39,7 +77,7 @@ function handleKeyPress(event) {
   }
   //1単語打ち切ったとき
   if (enteredChar.length == targetWord.romaji.length) {
-    isWordSet = false;
+    (isWordSet = false), (isNextWordSet = false);
     socket.emit("nextWord_to_server");
   }
 }
@@ -56,11 +94,17 @@ function draw() {
   context.fillStyle = "white";
   context.font = "40px Arial";
   if (!isWordSet) {
-    context.fillText("問題読込中…", 200, 100);
+    context.fillText("問題読込中…", 200, 150);
   } else {
-    context.fillText(targetWord.jp, 200, 100);
-    context.fillText(targetWord.romaji, 200, 150);
-    context.fillText(enteredChar + " |", 200, 200);
+    context.fillText(targetWord.jp, 200, 150);
+    context.fillText(targetWord.romaji, 200, 200);
+    context.fillText(enteredChar + "|", 200, 250);
+  }
+  context.font = "25px Arial";
+  if (!isNextWordSet) {
+    context.fillText("問題読込中…", 200, 330);
+  } else {
+    context.fillText("NEXT => " + nextTargetWord.jp, 200, 330);
   }
 }
 
@@ -71,4 +115,75 @@ function gameLoop() {
     draw();
     requestAnimationFrame(gameLoop);
   }
+}
+
+// ログインフォームの作成
+function createLoginForm() {
+  let loginForm = document.createElement("form");
+  loginForm.id = "login_form";
+  loginForm.name = "loginForm";
+  let userNameInput = document.createElement("input");
+  userNameInput.type = "text";
+  userNameInput.name = "userName";
+  let loginButton = document.createElement("button");
+  loginButton.type = "button";
+  loginButton.innerText = "ログイン";
+  loginButton.onclick = login;
+  loginForm.appendChild(userNameInput);
+  loginForm.appendChild(loginButton);
+  document.body.appendChild(loginForm);
+}
+
+// ログイン処理
+function login() {
+  let form = document.forms.loginForm;
+  let userName = form.elements.userName.value;
+  if (userName == "") {
+    alert("ユーザー名を入力してください");
+  } else {
+    document.getElementById("login_form").style.display = "none";
+    socket.emit("login_to_server", { userName: userName });
+  }
+}
+
+//ゲーム描画領域の作成
+function createGameCanvas() {
+  (canvas = document.createElement("canvas")), (canvas.id = "game_canvas");
+  (canvasWrapper = document.createElement("div")),
+    (canvasWrapper.id = "canvas_wrapper");
+  context = canvas.getContext("2d");
+  canvasWrapper.appendChild(canvas);
+  document.body.appendChild(canvasWrapper);
+  (canvas.width = 800), (canvas.height = 400);
+}
+
+//ロビーのログを作成
+function createLobbyLog() {
+  let log = document.createElement("ul");
+  log.id = "lobby_log";
+  document.body.appendChild(log);
+}
+
+//ロビーのログを更新
+function updateLobbyLog(message) {
+  let log = document.getElementById("lobby_log");
+  let li = document.createElement("li");
+  li.innerText = message;
+  log.appendChild(li);
+}
+
+//対戦待ちボタンの作成
+function createBattleWaitButton() {
+  let button = document.createElement("button");
+  button.id = "battle_wait_button";
+  button.innerText = "対戦相手を待つ";
+  button.onclick = battleWait;
+  document.body.appendChild(button);
+}
+
+//対戦相手待ち処理
+function battleWait() {
+  document.getElementsByTagName("h1")[0].innerText = "対戦相手を待っています…";
+  document.getElementById("battle_wait_button").style.display = "none";
+  socket.emit("battleWait_to_server");
 }
